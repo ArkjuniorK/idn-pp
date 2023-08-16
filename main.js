121c82384a3f6034eaca166f0430eab2ecbb497b
created.arkjuniork <created.arkjuniork@gmail.com>
2023-08-16 15:15:10 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,168 @@
+/**
+ * @copyright 2023 ArkjuniorK
+ * This is an example how to train a neural network using ml5.js (https://ml5js.org/), many thanks to ml5.js team for this
+ * helpful library and Winastwan Gora (https://www.kaggle.com/winastwangora) for the datasets which could
+ * be viewed and downloaded here https://www.kaggle.com/datasets/winastwangora/proyeksi-jumlah-penduduk-indonesia-jenis-kelamin.
+ *
+ * TODO:
+ * - Add interface to frontend []
+ * - Deploy to Github Pages []
+ *
+ */
+
+import provinces from "./provinces.js";
+
+const formComponent = document.querySelector("#form");
+const readyComponent = document.querySelector("#ready")
+const valueComponent = document.querySelector("#result-value")
+const loadingComponent = document.querySelector("#loading")
+const yearSelectorComponent = document.querySelector("#select-year")
+const provinceSelectorComponent = document.querySelector("#select-province")
+
+// options for ml5 neuralNetwork method
+const options = {
+    dataUrl: 'datasets.csv',
+    task: 'regression',
+    inputs: ['kode', 'tahun', 'kelamin'],
+    outputs: ['jumlah'],
+    debug: true, // TODO: disable this on deployment
+}
+
+// create neural network instance based on given
+// options then execute the callback
+const nn = ml5.neuralNetwork(options, dataLoaded)
+
+/**
+ * This is a callback that would be trigger
+ * inside the neuralNetwork function that responsible
+ * with two things:
+ * 1. Normalize input data
+ * 2. Train the model
+ */
+function dataLoaded() {
+    nn.normalizeData();
+    trainModel();
+}
+
+/**
+ * Train the model, specify the epochs and batchSize if necessary.
+ * After training execute the doneTraining callback.
+ */
+function trainModel() {
+    const trainOptions = { epochs: 59 };
+    nn.train(trainOptions, doneTraining);
+}
+
+/**
+ * This function would be trigger right after training the model.
+ * In here all operation would primarily use to modify the DOM.
+ */
+function doneTraining() {
+    // insert data into select
+    yearSelectorComponent.append(...getYears());
+    provinceSelectorComponent.append(...getProvinces());
+
+    // toggle components
+    readyComponent.classList.toggle("d-none");
+    loadingComponent.classList.toggle("d-none");
+}
+
+
+/**
+ * Generate an array of option tag
+ * for year selector up to 30 years forward.
+ *
+ * @returns {Node[]}
+ */
+function getYears() {
+    const currentYear  = new Date(Date.now()).getFullYear()
+    const yearLength   = 30;
+
+    let yearOptions = [];
+    let i = 0;
+
+    while(i <= yearLength) {
+        const year = currentYear + i
+        const node = createOptionNode(year, year)
+        yearOptions.push(node)
+        i++;
+    }
+
+    return yearOptions
+}
+
+/**
+ * Generate an array of option tag
+ * for province selector
+ *
+ * @returns {Node[]}
+ */
+function getProvinces() {
+    let provinceOptions = []
+
+    for (const province of provinces) {
+        const node = createOptionNode(province.provinsi, province.kode)
+        provinceOptions.push(node)
+    }
+
+    return provinceOptions;
+}
+
+
+/**
+ * Create an option node with its text and attribute.
+ *
+ * @param data {String|number}
+ * @param value {String|number}
+ * @returns {HTMLOptionElement}
+ */
+function createOptionNode(data, value) {
+    const node= document.createElement("option");
+    const text = document.createTextNode(data.toString());
+    const attribute = document.createAttribute("value");
+
+    attribute.value = value.toString();
+    node.setAttributeNode(attribute);
+    node.appendChild(text);
+
+    return node;
+}
+
+
+formComponent.addEventListener('submit', onSubmit)
+
+/**
+ * Handle submit button action. It would get the value
+ * of each input then combine it to inputs that ml5 would use to predict.
+ *
+ * @param e
+ */
+function onSubmit(e) {
+    e.preventDefault();
+
+    const year = parseInt(formComponent.elements["year"].value);
+    const gender = parseInt(formComponent.elements["gender"].value);
+    const province = parseInt(formComponent.elements["province"].value);
+
+    const inputs = {'kode': province, 'tahun': year, 'kelamin': gender};
+    nn.predict(inputs, handleResults);
+}
+
+
+/**
+ * Handle the result of neural network prediction.
+ * If an error is occurred then display the error message,
+ * otherwise display the result.
+ *
+ * @param error
+ * @param results
+ */
+function handleResults(error, results) {
+   if (error) {
+       console.error(error);
+       return
+   }
+
+   const value = results[0].value.toFixed(0)
+   valueComponent.innerHTML = value.toString()
+}
---
+++ provinces.js
@@ -0,0 +1,136 @@
+const provinces = [
+	{
+		"kode": 11,
+		"provinsi": "Aceh"
+	},
+	{
+		"kode": 12,
+		"provinsi": "Sumatera Utara",
+	},
+	{
+		"kode": 13,
+		"provinsi": "Sumatera Barat",
+	},
+	{
+		"kode": 14,
+		"provinsi": "Riau",
+	},
+	{
+		"kode": 15,
+		"provinsi": "Jambi",
+	},
+	{
+		"kode": 16,
+		"provinsi": "Sumatera Selatan",
+	},
+	{
+		"kode": 17,
+		"provinsi": "Bengkulu",
+	},
+	{
+		"kode": 18,
+		"provinsi": "Lampung",
+	},
+	{
+		"kode": 19,
+		"provinsi": "Bangka Belitung",
+	},
+	{
+		"kode": 21,
+		"provinsi": "Kepulauan Riau",
+	},
+	{
+		"kode": 31,
+		"provinsi": "DKI Jakarta",
+	},
+	{
+		"kode": 32,
+		"provinsi": "Jawa Barat",
+	},
+	{
+		"kode": 33,
+		"provinsi": "Jawa Tengah",
+	},
+	{
+		"kode": 34,
+		"provinsi": "DI Yogyakarta",
+	},
+	{
+		"kode": 35,
+		"provinsi": "Jawa Timur",
+	},
+	{
+		"kode": 36,
+		"provinsi": "Banten",
+	},
+	{
+		"kode": 51,
+		"provinsi": "Bali",
+	},
+	{
+		"kode": 52,
+		"provinsi": "Nusa Tenggara Barat",
+	},
+	{
+		"kode": 53,
+		"provinsi": "Nusa Tenggara Timur",
+	},
+	{
+		"kode": 61,
+		"provinsi": "Kalimantan Barat",
+	},
+	{
+		"kode": 62,
+		"provinsi": "Kalimantan Tengah",
+	},
+	{
+		"kode": 63,
+		"provinsi": "Kalimantan Selatan",
+	},
+	{
+		"kode": 64,
+		"provinsi": "Kalimantan Timur",
+	},
+	{
+		"kode": 71,
+		"provinsi": "Sulawesi Utara",
+	},
+	{
+		"kode": 72,
+		"provinsi": "Sulawesi Tengah",
+	},
+	{
+		"kode": 73,
+		"provinsi": "Sulawesi Selatan",
+	},
+	{
+		"kode": 74,
+		"provinsi": "Sulawesi Tenggara",
+	},
+	{
+		"kode": 75,
+		"provinsi": "Gorontalo",
+	},
+	{
+		"kode": 76,
+		"provinsi": "Sulawesi Barat",
+	},
+	{
+		"kode": 81,
+		"provinsi": "Maluku",
+	},
+	{
+		"kode": 82,
+		"provinsi": "Maluku Utara",
+	},
+	{
+		"kode": 91,
+		"provinsi": "Papua Barat",
+	},
+	{
+		"kode": 94,
+		"provinsi": "Papua",
+	},
+]
+
+export default provinces;
